6445100acaec414eb947a069ac53252f506eb7ce
eeee0717 <chentao020717Work@outlook.com>
2024-01-19 21:43:46 +0800

    feat:有道智云设置页面完成

--- SubtitleTranslator/SubtitleTranslator/Helpers/YoudaoTranslator.cs
+++ SubtitleTranslator/SubtitleTranslator/Helpers/YoudaoTranslator.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,25 +15,24 @@ public class YoudaoTranslator : ITranslator
   private Dictionary<string, string> _postBody;
   private const string Url = "https://openapi.youdao.com/api";
 
-  public YoudaoTranslator()
-  {
-  }
 
   private void Initialize(string text)
   {
-    string appKey = "3d8ed64d0bbcb811";
-    var appSecret = "8lLzxZkd5oeT93pSMmf5UbvTclvSoLFg";
+    ConfigFileHelper configFileHelper = new();
+
+    var appId = configFileHelper.ProviderOptions.YoudaoProviderOptions.AppId;
+    var appKey = configFileHelper.ProviderOptions.YoudaoProviderOptions.AppKey;
     TimeSpan ts = (DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc));
     long millis = (long)ts.TotalMilliseconds;
 
     string curtime = Convert.ToString(millis / 1000);
     string salt = DateTime.Now.Millisecond.ToString();
 
-    string signStr = appKey + Truncate(text) + salt + curtime + appSecret;
+    string signStr = appId + Truncate(text) + salt + curtime + appKey;
     string sign = ComputeHash(signStr, SHA256.Create());
     _postBody = new Dictionary<string, string>
     {
-      { "appKey", appKey },
+      { "appKey", appId },
       { "salt", salt },
       { "sign", sign },
       { "signType", "v3" },
@@ -45,12 +43,12 @@ public class YoudaoTranslator : ITranslator
   public async Task<string> Translate(string text, string sourceLanguage, string targetLanguage)
   {
     if (sourceLanguage == "zh") sourceLanguage = "zh-CHS";
-    if (sourceLanguage == "zh-TW") sourceLanguage = "zh-CHT";    
+    if (sourceLanguage == "zh-TW") sourceLanguage = "zh-CHT";
     if (targetLanguage == "zh") sourceLanguage = "zh-CHS";
     if (targetLanguage == "zh-TW") sourceLanguage = "zh-CHT";
     Initialize(text);
     _postBody.Add("from", sourceLanguage);
-    _postBody.Add("to", "zh-CHS");
+    _postBody.Add("to", targetLanguage);
     _postBody.Add("q", System.Web.HttpUtility.UrlEncode(text));
     string resp = await Post();
     JObject jObject = JObject.Parse(resp);
@@ -84,7 +82,7 @@ public class YoudaoTranslator : ITranslator
 
     var data = Encoding.UTF8.GetBytes(builder.ToString());
     req.ContentLength = data.Length;
-    
+
     using var reqStream = req.GetRequestStream();
     reqStream.Write(data, 0, data.Length);
     reqStream.Close();
@@ -107,37 +105,5 @@ public class YoudaoTranslator : ITranslator
 
     int len = q.Length;
     return len <= 20 ? q : q.Substring(0, 10) + len + q.Substring(len - 10, 10);
-  }
-
-  private static bool SaveBinaryFile(WebResponse response, string FileName)
-  {
-    string filePath = FileName + DateTime.Now.Millisecond + ".mp3";
-    bool value = true;
-    byte[] buffer = new byte[1024];
-
-    try
-    {
-      if (File.Exists(filePath))
-        File.Delete(filePath);
-      Stream outStream = File.Create(filePath);
-      Stream inStream = response.GetResponseStream();
-
-      int l;
-      do
-      {
-        l = inStream.Read(buffer, 0, buffer.Length);
-        if (l > 0)
-          outStream.Write(buffer, 0, l);
-      } while (l > 0);
-
-      outStream.Close();
-      inStream.Close();
-    }
-    catch
-    {
-      value = false;
-    }
-
-    return value;
   }
 }
--- SubtitleTranslator/SubtitleTranslator/Providers/ProviderOptions.cs
+++ SubtitleTranslator/SubtitleTranslator/Providers/ProviderOptions.cs
@@ -3,4 +3,5 @@ namespace SubtitleTranslator.Providers;
 public class ProviderOptions
 {
   public TencentProviderOptions TencentProviderOptions { get; set; }
+  public YoudaoProviderOptions YoudaoProviderOptions { get; set; }
 }
